c8c334c6577e08db7670f01d01a7fb0445cf1742
Reizhea <nawangdorjee798@gmail.com>
2025-04-17 22:26:47 +0530

    Frontend integration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    https: {
-      key: fs.readFileSync("./localhost-key.pem"),
-      cert: fs.readFileSync("./localhost.pem"),
-    },
-    host: "localhost",
-    port: 5173,
+    host: true, // allows access via network
+    port: 5173, // optional
   },
 });
